94a7f7e374c6d15b5f4fa3b1b48ba352610bb606
Efox6179 <95189308+Efox6179@users.noreply.github.com>
2022-05-21 09:32:05 -0500

    working finally

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
 -- DROP DATABASE
-DROP DATABASE IF EXISTS ecommerce_db;
+DROP DATABASE IF EXISTS ecommerce1_db;
 
 -- CREATE DATABASE
-CREATE DATABASE ecommerce_db;
+CREATE DATABASE ecommerce1_db;
 
